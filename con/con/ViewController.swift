98f52391d6f26a7ef568490dd8d5bd2fa8f27b82
Ying <ying@interaxon.ca>
2019-01-08 17:07:28 -0500

    create project

---
+++ con/con/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  con
+//
+//  Created by Ying  on 2019-01-08.
+//  Copyright © 2019 Ying . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
