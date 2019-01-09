73b6e517529c78ea94de9f106ed0f58d00ea3894
Ying <ying@interaxon.ca>
2019-01-09 17:20:05 -0500

    draw card

--- con/con/ViewController.swift
+++ con/con/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func card1(_ sender: UIButton) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
