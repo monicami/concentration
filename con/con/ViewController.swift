2572a6c8865ebe74518bcab3115a5c4315a15c89
Ying <ying@interaxon.ca>
2019-01-16 17:14:53 -0500

    add more cards

--- con/con/ViewController.swift
+++ con/con/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBAction func card1(_ sender: UIButton) {
-        
+    var flipcount = 0 {
+        didSet{
+            flipcountLable.text = "Flip count: \(flipcount)"
+        }
+    }
+    @IBOutlet weak var flipcountLable: UILabel!    
+    @IBOutlet var UIButtons: [UIButton]!
+    @IBAction func touchcard(_ sender: UIButton) {
+        flipcount += 1
+        flipcard(withEmoji: "🎃", on: sender)
     }
     
+    func flipcard(withEmoji emoji:String, on button: UIButton) {
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            print("the emoji is\(emoji)")
+        }else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
     
 
 }
